8f84dd8f1e23997fcc16e31508144fd18f528dab
migomat <35002820+migomat@users.noreply.github.com>
2018-01-04 23:07:15 +0100

    Add files via upload

--- gamepad.py
+++ gamepad.py
@@ -1,54 +1,121 @@
 import evdev
+import serial
+from bitarray import bitarray
+
+arduino_port = serial.Serial('/dev/ttyUSB0',9600, timeout=3.0)
 from evdev import InputDevice, categorize, ecodes, KeyEvent
 devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+import RPi.GPIO as GPIO
+
 for device in devices:
     print(device.fn, device.name, device.phys)
 
 gamepad = InputDevice('/dev/input/event0')
+dataframe = bitarray('00000000')
 
-print('capabilites:')
+
+#print('capabilites:')
 #print(gamepad.capabilities())
-print('capabilites: verbose=True')
+#print('capabilites: verbose=True')
 #print(gamepad.capabilities(verbose=True))
 #print(gamepad.leds(verbose=True))#wyswietla liste dostepnych led ktorymi mozna mrugac
 #print(gamepad.active_keys(verbose=True))
 for event in gamepad.read_loop():
+    #print(event)
+   # try:
+    #    arduinofeedback=arduino_port.readline()
+    #    print('value_readed:')
+    #    print(arduinofeedback)
+   # except:
+    #    pass
     if event.code == 16:
         if event.value==-1:
             print('dpad_left')
+            dataframe=dataframe|bitarray('00000100')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
         elif event.value==1:
             print('dpad_right')
+            dataframe=dataframe|bitarray('00001000')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
+        elif event.value==0:
+            print('dpad_rl_released')
+            dataframe=dataframe & bitarray('11110011')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
     elif event.code == 17:
         if event.value==-1:
             print('dpad_up')
+            dataframe=dataframe | bitarray('00000001')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
         elif event.value==1:
             print('dpad_down')
-    elif event.value==1:
-        if event.code==288:
-            print('triangle')
-        elif event.code==289:
-            print('circle')
-        elif event.code==291:
+            dataframe=dataframe | bitarray('00000010')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
+        elif event.value==0:
+            print('dpad_updown_released')
+            dataframe=dataframe & bitarray('11111100')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
+    
+    elif event.code==288:
+        print('triangle')
+    elif event.code==289:
+        print('circle')
+    elif event.code==291:
+        if event.value==1:
             print('square')
-        elif event.code==290:
+            dataframe=dataframe | bitarray('00000010')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
+        elif event.value==0:
+            print('square_released')
+            dataframe=dataframe & bitarray('11111100')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
+    elif event.code==290:
+        if event.value==1:
             print('cross')
-        elif event.code==297:
+            dataframe=dataframe | bitarray('00000001')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
+        elif event.value==0:
+            print('cross_released')
+            dataframe=dataframe & bitarray('11111100')
+            print(dataframe)
+            arduino_port.write(dataframe.tobytes())
+            
+    elif event.code==297:
+        if event.value==1:
             print('start')
-        elif event.code==296:
+            dataframe = bitarray('00000000')
+            arduino_port.write(dataframe.tobytes())
+    elif event.code==296:
+        if event.value==1:
             print('select')
-        elif event.code==292:
+    elif event.code==292:
+        if event.value==1:
             print('l1')
-        elif event.code==294:
+    elif event.code==294:
+        if event.value==1:
             print('l2')
-        elif event.code==293:
+    elif event.code==293:
+        if event.value==1:
             print('r1')
-        elif event.code==295:
+    elif event.code==295:
+        if event.value==1:
             print('r2')
-        elif event.code==299:
+    elif event.code==299:
+        if event.value==1:
             print('r_tog')
-        elif event.code==298:
+    elif event.code==298:
+        if event.value==1:
             print('l_tog')
-        
+  #  print(dataframe)
+ #   arduino_port.close()
    # else:
         #print('unrecognizable button')
         #print(event.code)
---
+++ gamepad_v1.ino
@@ -0,0 +1,197 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the Uno and
+  Leonardo, it is attached to digital pin 13. If you're unsure what
+  pin the on-board LED is connected to on your Arduino model, check
+  the documentation at http://arduino.cc
+
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+ */
+
+  int fwd_pin=2;
+  int rwd_pin=3;
+  int lh_pin=5;
+  int rh_pin=4;
+  int read_rasp=0;
+  int incomingByte = 0;
+  byte b0;
+  byte b1;
+  byte b2;
+  byte b3;
+  byte b4;
+  byte b5;
+  byte b6;
+  byte b7;
+  
+  
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+ 
+Serial.begin(9600);
+ 
+
+  pinMode(fwd_pin, OUTPUT);
+  pinMode(rwd_pin, OUTPUT);
+  pinMode(lh_pin, OUTPUT);
+  pinMode(rh_pin, OUTPUT);
+  pinMode(13,OUTPUT);
+  digitalWrite(fwd_pin, LOW);
+  digitalWrite(rwd_pin, LOW);
+  digitalWrite(lh_pin, LOW);
+  digitalWrite(rh_pin, LOW);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  while(Serial.available()>0)
+  {
+  incomingByte = Serial.read();
+  b0=bitRead(incomingByte,0);
+  b1=bitRead(incomingByte,1);
+  b2=bitRead(incomingByte,2);
+  b3=bitRead(incomingByte,3);
+  b4=bitRead(incomingByte,4);
+  b5=bitRead(incomingByte,5);
+  b6=bitRead(incomingByte,6);
+  b7=bitRead(incomingByte,7);
+ 
+ digitalWrite(fwd_pin, b0);
+ digitalWrite(rwd_pin, b1);
+ digitalWrite(lh_pin, b2);
+ digitalWrite(rh_pin, b3);
+ 
+ 
+ // say what you got:
+ // Serial.print("I received: ");
+  //Serial.println(incomingByte, BIN);
+ /* if (b0==1){
+  mv_fwd();
+  } 
+  if (b0==0){
+   stp_fwd();
+  } 
+  else if (read_rasp==2){
+  mv_rwd();
+  }   
+  
+  else if (read_rasp==3){
+  mv_lh();
+  }  
+  
+  else if (read_rasp==4){
+  mv_rh();
+  }  
+  
+  else if (read_rasp==5){
+  mv_fwdlh();
+  }  
+  
+   else  if (read_rasp==6){
+  mv_fwdrh();
+  }  
+  
+    else if (read_rasp==7){
+  mv_rwdlh();
+  }  
+  
+    else   if (read_rasp==8){
+  mv_rwdrh();
+  }  
+  else   if (read_rasp==9){
+  mv_stop();
+  }
+*/
+
+}
+
+}
+
+
+void mv_fwd(){
+  digitalWrite(fwd_pin, HIGH);
+  digitalWrite(rwd_pin, LOW);
+  digitalWrite(13, HIGH);
+
+}
+
+void mv_rwd()
+{
+  digitalWrite(rwd_pin, HIGH);
+  digitalWrite(fwd_pin, LOW);
+  digitalWrite(13, LOW);
+}
+void mv_lh()
+{
+  digitalWrite(lh_pin, HIGH);
+  digitalWrite(rh_pin, LOW);
+  delay(200);
+}
+void mv_rh()
+{
+  digitalWrite(rh_pin, HIGH);
+  digitalWrite(lh_pin, LOW);
+  delay(200);
+}
+
+void mv_fwdlh()
+{
+  digitalWrite(fwd_pin, HIGH);
+  digitalWrite(lh_pin, HIGH);
+   digitalWrite(rwd_pin, LOW);
+  digitalWrite(rh_pin, LOW);
+}
+
+void mv_fwdrh()
+{
+  digitalWrite(fwd_pin, HIGH);
+  digitalWrite(rh_pin, HIGH);
+  digitalWrite(rwd_pin, LOW);
+  digitalWrite(lh_pin, LOW);
+}
+
+void mv_rwdlh()
+{
+  digitalWrite(rwd_pin, HIGH);
+  digitalWrite(lh_pin, HIGH);
+   digitalWrite(fwd_pin, LOW);
+  digitalWrite(rh_pin, LOW);
+}
+
+void mv_rwdrh()
+{
+  digitalWrite(rwd_pin, HIGH);
+  digitalWrite(rh_pin, HIGH);
+   digitalWrite(fwd_pin, LOW);
+  digitalWrite(lh_pin, LOW);
+}
+
+void mv_stop()
+{
+  digitalWrite(rwd_pin, LOW);
+  digitalWrite(fwd_pin, LOW);
+  digitalWrite(rh_pin, LOW);
+  digitalWrite(lh_pin, LOW);
+}
+
+void stp_fwd()
+{
+    digitalWrite(fwd_pin, LOW);
+}
+void stp_rwd()
+{
+    digitalWrite(rwd_pin, LOW);
+}
+void stp_lh()
+{
+    digitalWrite(lh_pin, LOW);
+}
+void stp_rh()
+{
+    digitalWrite(rh_pin, LOW);
+}
